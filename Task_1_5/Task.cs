876c3fd8da30c90bec1177556f58633813a71d0b
Sveta <bogatovasvetlanau@gmail.com>
2021-05-03 18:03:13 +0300

    Task 5 solution

---
+++ Task_1_5/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_1_5
+{
+    public class Task
+    {
+        public string TaskName { get; set; }
+        public string Priority { get; set; }
+        public string Complexity { get; set; }
+    }
+}
